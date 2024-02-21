12de957926253d37135c757c647a69491dc705d1
Lainezz <diegolinaresmojacar@gmail.com>
2024-02-21 09:34:31 +0100

    adicion de codigo para clase

--- grupo1/Ejer5.py
+++ grupo1/Ejer5.py
@@ -1,4 +1,4 @@
 def alojamientos_de_anfitriones(lista_alojamientos):
     """
-    Crear una función que reciba la lista de alojamientos y devuelva un diccionario con los anfitriones y el número de alojamientos que posee cada uno.s
+    Crear una función que reciba la lista de alojamientos y muestre los anfitriones y el número de alojamientos que posee cada uno.
     """
