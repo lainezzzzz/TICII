e13c5584717532e8ba24cf8040931c910e7d1dde
Ainhoa0306 <arommar0312@g.educaand.es>
2024-02-29 10:28:08 +0100

    cambios

--- grupo1/Ejer5.py
+++ grupo1/Ejer5.py
@@ -1,5 +1,15 @@
-def alojamientos_de_anfitriones(lista_alojamientos):
-    """
-    Crear una función que reciba la lista de alojamientos y muestre los anfitriones y el número de alojamientos que posee cada uno.
-    """
-    
+# def alojamientos_de_anfitriones(lista_alojamientos):
+    # """
+    # Crear una función que reciba la lista de alojamientos y muestre los anfitriones y el número de alojamientos que posee cada uno.
+    # """
+lista_alojamientos = {"id":"96033",
+        "host_id":"510467",
+        "neighbourhood":"Este",
+        "price":"53"}
+
+print(lista_alojamientos["host_id"])
+
+    # Recorrer la lista. Cada posición de la lista es un diccionario
+    # Para cada diccionario, accedes a host_id... ese host_id lo añades a un diccionario propio
+    # AL recorrer la lista, cuentas cuántas veces aparece ese host id
+    # Guardas cuántas veces aparece y lo almacenas en el diccionario que has creado antes
